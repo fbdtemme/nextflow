7b994b13312770bd8a67ecb5761e71d390f16def
fbdtemme <florian.detemmerman@lizard.bio>
2022-01-14 14:41:51 +0100

    Debuging work

--- modules/nf-commons/src/main/nextflow/Const.groovy
+++ modules/nf-commons/src/main/nextflow/Const.groovy
@@ -58,12 +58,12 @@ class Const {
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1642164129983
+    static public final long APP_TIMESTAMP = 1642167581661
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 5664
+    static public final int APP_BUILDNUM = 5665
 
     /**
      * The app build time string relative to UTC timezone
--- plugins/nf-azure/src/main/nextflow/cloud/azure/nio/AzFileSystem.groovy
+++ plugins/nf-azure/src/main/nextflow/cloud/azure/nio/AzFileSystem.groovy
@@ -371,6 +371,8 @@ class AzFileSystem extends FileSystem {
             int count=0
             while( values.hasNext() ) {
                 BlobItem blob = values.next()
+
+                log.debug "GuessPath: Name: ${blob.getName()}"
                 if( blob.name == name )
                     exists = true
                 else if( blob.name.startsWith(name) && blob.name.charAt(name.length())==SLASH ) {
@@ -409,7 +411,6 @@ class AzFileSystem extends FileSystem {
     @PackageScope
     void copy(AzPath source, AzPath target) {
         log.debug "Azure Blob storage copy operation src=${source.toString()} -> dst=${target.toString()}}"
-        log.debug "Azure Blob is target a directory: ${target.isDirectory()}"
 
         SyncPoller<BlobCopyInfo, Void> pollResponse =
                 target.blobClient().beginCopy( source.blobClient().getBlobUrl(), null )
