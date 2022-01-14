2836b38f63fa91d03c42e23a3575a39d9a10ca95
fbdtemme <florian.detemmerman@lizard.bio>
2022-01-14 15:31:29 +0100

    Debuging work

--- plugins/nf-azure/src/main/nextflow/cloud/azure/nio/AzFileAttributes.groovy
+++ plugins/nf-azure/src/main/nextflow/cloud/azure/nio/AzFileAttributes.groovy
@@ -55,6 +55,7 @@ class AzFileAttributes implements BasicFileAttributes {
 
     AzFileAttributes(BlobClient client) {
         final props = client.getProperties()
+        log.info "AzFileAttributes: ${props.toString()}"
         objectId = "/${client.containerName}/${client.blobName}"
         creationTime = time(props.getCreationTime())
         updateTime = time(props.getLastModified())
